c787941853abde859701c39406c8e90001548d8e
Bill Gestrich <bill.gestrich@brandingbrand.com>
2013-05-17 21:45:31 -0400

    Initial page created with image

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require("express");
+var hbs = require("hbs");
 var app = express();
 app.use(express.logger());
+app.use(express.static(__dirname + '/public'));
+
+console.log(app.settings);
+//app.set('views', __dirname + '/views');
+app.engine('html', hbs.__express);
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+	response.render('index.html');
 });
 
 var port = process.env.PORT || 5000;
