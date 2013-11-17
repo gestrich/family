020eb773b258f89298c50d6cad8efd8b6b630ddd
Bill Gestrich <wgestrich@gmail.com>
2013-11-17 12:47:18 -0600

    Undefined check change 9

--- handlers/home.js
+++ handlers/home.js
@@ -70,7 +70,9 @@ console.log("req.query = " + JSON.stringify(req.query));
 
             setEncoding();
 
-            if( name != 'undefined' && commentText != 'undefined' && name.length > 0 && commentText.length>0){
+            if( name != 'undefined' && commentText != 'undefined' 
+              && typeof name != 'undefined && typeof commentText != 'undefined 
+              && name.length > 0 && commentText.length>0){
 console.log("name = " + name);
 console.log("commentText = " + commentText);
 console.log("commentText = " + commentText);
